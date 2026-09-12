08687b4db39faa3a7ea60cf0c9ea9a5ccab4531a
fjsipil-cmd <fjsipil@gmail.com>
2026-09-12 22:27:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sigap-gempa-flores-v20-bottom-fixed-nav';
+const CACHE='sigap-gempa-flores-v21-desktop-bottom-only';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./logo-manggarai.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
