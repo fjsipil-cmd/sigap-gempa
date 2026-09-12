199b9d8736954b0c9d33a369005beb49a0d49db8
fjsipil-cmd <fjsipil@gmail.com>
2026-09-12 20:20:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sigap-gempa-flores-v15-damage-11px-extra-muted';
+const CACHE='sigap-gempa-flores-v16-admin-validation-success';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./logo-manggarai.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
